6a3a3b17c21f32f837cd97211a5e8381ad7c7376
blackdanamite <seagrendaniel562@gmail.com>
2020-04-20 18:02:04 -0700

    fetch stories from Hacker News API and add custom error for server error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+
 class Index extends React.Component {
+  static async getInitialProps() {
+    let stories;
+    try {
+      const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+      stories = await response.json();
+    } catch(err) {
+      console.log(err);
+      stories = [];
+    }
+
+    return {stories}
+  }
+
   render() {
+    const {stories} = this.props;
+    if(stories.length === 0) {
+      return <Error statusCode={503}/>
+    }
+
     return(
       <div>Hacker Next</div>
     )
