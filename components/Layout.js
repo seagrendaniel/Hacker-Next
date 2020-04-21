b086bc165e6741eafad271457a3c5000234e63f8
blackdanamite <seagrendaniel562@gmail.com>
2020-04-20 20:00:07 -0700

    add story page and styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,20 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
-const Layout = ({ children }) => (
-  <div className="container">
-    <nav>
-      <Link href="/">
-        <a><span className="main-title">Hacker Next</span></a>
-      </Link>
-    </nav>
+const Layout = ({ children, title, description }) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Head>
+    <div className="container">
+      <nav>
+        <Link href="/">
+          <a><span className="main-title">Hacker Next</span></a>
+        </Link>
+      </nav>
       {children}
-    <style jsx>{`
+      <style jsx>{`
       .container {
         max-width: 800px;
         margin: 0 auto;
@@ -29,12 +35,13 @@ const Layout = ({ children }) => (
         font-weight: bold;
       }
     `}</style>
-    <style global jsx>{`
+      <style global jsx>{`
       body{
         background: white;
         font-family: Verdana, Geneva, sans-serif;
       }
     `}</style>
+    </div>
   </div>
 )
 
